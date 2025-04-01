26c4570011c29a06aba761b42e0b7464b56c084d
Thean Yee Sin <yeesinthean@outlook.com>
2025-04-01 14:52:46 +0800

    feat: calculate distance

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,42 @@
 import "./App.css";
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import {
+  MapContainer,
+  Marker,
+  Polyline,
+  Popup,
+  TileLayer,
+  useMapEvents,
+} from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import { LeafletControlGeocoder } from "./components";
+import { useState } from "react";
+import L, { LeafletMouseEvent, LatLng } from "leaflet";
 
 const App = () => {
+  const [startLocation, setStartLocation] = useState<LatLng | null>(null);
+  const [endLocation, setEndLocation] = useState<LatLng | null>(null);
+  const [distance, setDistance] = useState<string | null>(null);
+
+  const LocationMarker: React.FC = () => {
+    useMapEvents({
+      click(e: LeafletMouseEvent) {
+        if (!startLocation) {
+          setStartLocation(new L.LatLng(e.latlng.lat, e.latlng.lng));
+        } else if (!endLocation) {
+          setEndLocation(new L.LatLng(e.latlng.lat, e.latlng.lng));
+        }
+      },
+    });
+    return null;
+  };
+
+  const calculateDistance = () => {
+    if (startLocation && endLocation) {
+      const dist = startLocation.distanceTo(endLocation) / 1000;
+      setDistance(dist.toFixed(2));
+    }
+  };
+
   return (
     <div style={{ height: "100vh", width: "100vw" }}>
       <MapContainer
@@ -16,13 +49,31 @@ const App = () => {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
+        <LocationMarker />
         <LeafletControlGeocoder />
-        <Marker position={[51.505, -0.09]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        {startLocation && (
+          <Marker position={startLocation}>
+            <Popup>Start Location</Popup>
+          </Marker>
+        )}
+        {endLocation && (
+          <Marker position={endLocation}>
+            <Popup>End Location</Popup>
+          </Marker>
+        )}
+        {startLocation && endLocation && (
+          <Polyline positions={[startLocation, endLocation]} />
+        )}
       </MapContainer>
+      <div>
+        <button
+          onClick={calculateDistance}
+          disabled={!startLocation || !endLocation}
+        >
+          Calculate Distance
+        </button>
+        {distance && <p>Distance: {distance} km</p>}
+      </div>
     </div>
   );
 };
