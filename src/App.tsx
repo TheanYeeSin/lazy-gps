6e31da461ef464da121aeee4125c612dbaaab862
Thean Yee Sin <yeesinthean@outlook.com>
2025-04-02 18:30:03 +0800

    update: finishing touches

--- src/App.tsx
+++ src/App.tsx
@@ -9,13 +9,16 @@ import {
 } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import { LeafletControlGeocoder } from "./components";
-import { useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import L, { LeafletMouseEvent, LatLng } from "leaflet";
 
 const App = () => {
   const [startLocation, setStartLocation] = useState<LatLng | null>(null);
   const [endLocation, setEndLocation] = useState<LatLng | null>(null);
   const [distance, setDistance] = useState<string | null>(null);
+
+  const startMarkerRef = useRef<L.Marker | null>(null);
+  const endMarkerRef = useRef<L.Marker | null>(null);
 
   const LocationMarker: React.FC = () => {
     useMapEvents({
@@ -37,12 +40,18 @@ const App = () => {
     }
   };
 
+  useEffect(() => {
+    calculateDistance();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [startLocation, endLocation]);
+
   return (
     <div style={{ height: "100vh", width: "100vw" }}>
+      <div>{distance && <p>Distance: {distance} km</p>}</div>
       <MapContainer
         center={[51.505, -0.09]}
         zoom={13}
-        scrollWheelZoom={false}
+        scrollWheelZoom={true}
         style={{ height: "100%", width: "100%" }}
       >
         <TileLayer
@@ -52,12 +61,36 @@ const App = () => {
         <LocationMarker />
         <LeafletControlGeocoder />
         {startLocation && (
-          <Marker position={startLocation}>
+          <Marker
+            position={startLocation}
+            draggable
+            ref={startMarkerRef}
+            eventHandlers={{
+              dragend: () => {
+                if (startMarkerRef.current) {
+                  const newPos = startMarkerRef.current.getLatLng();
+                  setStartLocation(newPos);
+                }
+              },
+            }}
+          >
             <Popup>Start Location</Popup>
           </Marker>
         )}
         {endLocation && (
-          <Marker position={endLocation}>
+          <Marker
+            position={endLocation}
+            draggable
+            ref={endMarkerRef}
+            eventHandlers={{
+              dragend: () => {
+                if (endMarkerRef.current) {
+                  const newPos = endMarkerRef.current.getLatLng();
+                  setEndLocation(newPos);
+                }
+              },
+            }}
+          >
             <Popup>End Location</Popup>
           </Marker>
         )}
@@ -65,15 +98,6 @@ const App = () => {
           <Polyline positions={[startLocation, endLocation]} />
         )}
       </MapContainer>
-      <div>
-        <button
-          onClick={calculateDistance}
-          disabled={!startLocation || !endLocation}
-        >
-          Calculate Distance
-        </button>
-        {distance && <p>Distance: {distance} km</p>}
-      </div>
     </div>
   );
 };
